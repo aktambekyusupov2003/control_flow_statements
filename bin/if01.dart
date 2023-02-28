1fa4a7a3e4d23789ca405236037eb53d9ba7963e
aktam <aktam36911@gmail.com>
2023-02-28 10:43:46 +0500

    uzbek

--- bin/if01.dart
+++ bin/if01.dart
@@ -1,5 +1,5 @@
 /*
-    Create function called func
+     Createfunction called func
     Create a function argument  called number of type int
     If the number is positive, increase it to 1, else leave unchanged.
     Args:
@@ -7,4 +7,17 @@
     Returns:
         a: a increased by 1 if positive, else unchanged.
  */
-void main() {}
+int func(int a) {
+  if (a > 0) {
+    return a += 1;
+  }
+
+  if (a < 0) {
+    return a = a;
+  }
+  return a;
+}
+
+void main() {
+  print(func(6));
+}
